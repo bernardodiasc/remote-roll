ff9ab3e4b0326bd7047685853ad8ec6a960844b2
Bernardo Dias <bernardodiasdacruz@gmail.com>
2024-01-25 11:35:16 -0300

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home () {
       <h1>REMOTE ROLL WEBSITE</h1>
 
       <h2>On-Demand content production Teams.</h2>
-      <p>Elevate your content with Remote Roll's elite producers/creatives, curating cinematic experiences for brands worldwide.</p>
+      <p>Elevate your content with Remote Roll&apos;s elite producers/creatives, curating cinematic experiences for brands worldwide.</p>
 
       <p>Scale your team with Senior Editors to Master content consistency.<br/>
       Master content consistency with Senior Editors.</p>
@@ -21,7 +21,7 @@ export default function Home () {
       <p>Adapt your production team to match your content needs. We customize teams based on your requirements.</p>
 
       <h3>The Perfect Match:</h3>
-      <p>Remote Roll customizes the perfect team for your company, whether it's a Creative Director, Producer, Editor, or Content Creator – you name it. For all your content production needs, we connect you with the right individuals to bring your vision to life.</p>
+      <p>Remote Roll customizes the perfect team for your company, whether it&apos;s a Creative Director, Producer, Editor, or Content Creator - you name it. For all your content production needs, we connect you with the right individuals to bring your vision to life.</p>
 
       <p>More than just a production company.</p>
 
@@ -29,7 +29,7 @@ export default function Home () {
       <p>Remote Roll is not limited to one location. We connect you with producers from all over the world to find the best location and pricing for your project. </p>
 
       <h3>Curating process:</h3>
-      <p>Every project has distinct needs, and securing the right professional is crucial. Remote Roll streamlines the process, connecting you with the precise skill set needed to elevate your company's content.</p>
+      <p>Every project has distinct needs, and securing the right professional is crucial. Remote Roll streamlines the process, connecting you with the precise skill set needed to elevate your company&apos;s content.</p>
 
       <h3>Content opportunities:</h3>
       <p>Move past the ordinary with the transformative power of video. When seamlessly integrated with the sales team, content production becomes a dynamic duo, driving  sales growth.</p>
